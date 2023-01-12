6d8784157d2c2337dc3375719475cf9feba63006
zehraghali <zehraghali@gmail.com>
2023-01-12 11:30:05 +0200

    säde

--- moduuli1/esimerkki.py
+++ moduuli1/esimerkki.py
@@ -1,2 +1,9 @@
-nimi = input("Anna koiran nimi: ")
-print("Koiran nimi on " + nimi)
+import math
+
+säde = float(input("Anna ympyrän säde sentteinä: "))
+pintaAla = math.pi * math.pow(säde, 2)
+
+print(f"Ympyrän, jonka säde on {säde:4.1f} pinta ala on {pintaAla:5.1f}")
+
+
+
