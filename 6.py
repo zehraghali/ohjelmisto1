f7f526949bb1e6001ba319d222fdd4db88c81919
zehraghali <zehraghali@gmail.com>
2023-01-18 13:47:38 +0200

    säde

---
+++ 6.py
@@ -0,0 +1,11 @@
+import math
+import random
+
+koodi1 = str(random.random())[2:5]
+print(koodi1)
+
+num1 = random.randint(1, 6)
+num2 = random.randint(1, 6)
+num3 = random.randint(1, 6)
+num4 = random.randint(1, 6)
+
