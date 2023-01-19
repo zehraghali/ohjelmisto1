8cc422117ea62b94dcc04fb37c3c777b8dca7761
zehraghali <zehraghali@gmail.com>
2023-01-19 13:29:47 +0200

    säde

---
+++ Käyttäjä.py
@@ -0,0 +1,16 @@
+käyttäjä = "python"
+salasana = "rules"
+kerrat = 0
+
+while kerrat < 5:
+    kerrat = kerrat + 1
+    print("kerrat")
+    user = input("käyttäjätunnus")
+    pas = input("salasana")
+    if käyttäjä == käyttäjä and salasana == pas:
+        print("Tervetuloa")
+        break
+    else:
+     print("Pääsy evätty!")
+     print("Kokeile uudestaan")
+kerrat = kerrat + 1
