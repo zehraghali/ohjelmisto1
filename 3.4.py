fdad641f1b23034d4dafe0fc28166b172cd977d9
zehraghali <zehraghali@gmail.com>
2023-01-23 12:43:29 +0200

    tehtävä

---
+++ 3.4.py
@@ -0,0 +1,5 @@
+vuosi = int(input("Anna vuosi"))
+if vuosi % 4 == 0 and (vuosi % 100 !=0 or vuosi % 400 == 0))
+    print("Tämä vuosi on karkausvuosi")
+else:
+    print("Tämä vuosi ei ole karkausvuosi")
--- suorakulmio.py
+++ suorakulmio.py
@@ -1,5 +1,5 @@
-kanta = int(input("Anna kanta: "))
-korkeus = int(input("Anna korkeus: "))
+kanta = float(input("Anna kanta: "))
+korkeus = float(input("Anna korkeus: "))
 piiri = kanta * 4
 korkeus = kanta * 2
 print(piiri)
