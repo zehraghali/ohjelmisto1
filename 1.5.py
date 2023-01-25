d528ba94c8b21cec8fe361823345c9dbea63932a
zehraghali <zehraghali@gmail.com>
2023-01-25 15:17:36 +0200

    tehtävä

---
+++ 1.5.py
@@ -0,0 +1,23 @@
+# 1:
+# lkm
+# summa = 0
+# toista lkm kertaa
+# heitä noppaa, päivitä summa
+
+# tulosta summa
+
+#2:
+# luvut ()
+# luku kysy lukuja whilen aiaällä, tallenna lvut-listaan
+
+# sort luvut,reverse=true
+# tulosta luvut(a:b)
+
+#4:
+# nimet = ()
+# for kierretään 5 kertaa, lisää nimet kaupunki nimet-listaan
+
+# for in
+# tulostaa kaupungin nimet
+
+
