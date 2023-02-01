5add78a484e5a48b92c2ed1e13d599be81caefdc
zehraghali <zehraghali@gmail.com>
2023-02-01 15:29:42 +0200

    tehtävä

---
+++ 7.3.py
@@ -0,0 +1,9 @@
+toiminto = input("Kirjoita uusi / haku / enter = lopeta: ")
+
+airports = {}
+toiminto = input("Kirjoita uusi / haku / enter = lopeta: ")
+airports ["EFHK"] = "Helsinki vantaa"
+airports["EFVA"] = "Vaasan lentokenttä"
+print(airports ["EFVA"])
+
+
