8e28ce77bd93659db854a200ae29fcd30d3c09e7
zehraghali <zehraghali@gmail.com>
2023-02-01 15:29:27 +0200

    tehtävä

---
+++ 7.1.py
@@ -0,0 +1,14 @@
+vuodenajat = ("talvi", "kevät", "kesä", "syksy")
+kuukausi = int(input("syötä kuukausi: "))
+if 1 <= kuukausi <=2:
+    print(vuodenajat[0])
+elif 3 <= kuukausi <= 5:
+    print(vuodenajat)[1]
+elif 6 <= kuukausi <8:
+    print(vuodenajat [2])
+elif 9 <= kuukausi <= 11:
+    print(vuodenajat[3])
+else:
+    print(vuodenajat[0])
+
+
