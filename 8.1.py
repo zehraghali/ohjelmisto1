5309138ed941f803f1c80998fd67bf757caa8687
zehraghali <zehraghali@gmail.com>
2023-03-01 11:27:59 +0200

    tehtävä

---
+++ 8.1.py
@@ -0,0 +1,24 @@
+import mysql.connector
+import mysql.connector
+
+yhteys = mysql.connector.connect(
+    host='127.0.0.1',
+    port= 3306,
+    database='flight_game',
+    user= 'root',
+    password='zehra,99',
+    automcommit=True
+    )
+
+"SELECt name FROM airport WHERE iso_country =  'FI';"
+
+def haelentokenttaMaasta(maa):
+    sql = f"SELECt name FROM airport WHERE iso_country = '{maa}';"
+    kursori = yhteys.cursor(dictionary=True)
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    return tulos
+kentat = haelentokenttaMaasta('FI')
+for rivi in kentat:
+    print(rivi['name'])
+
